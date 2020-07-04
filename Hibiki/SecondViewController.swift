be14d9bc68434a3cd5bb0c0cf79162e54b4940a9
suraj <i@surajsau.in>
2020-07-04 08:49:02 +0530

    Initial Commit

---
+++ Hibiki/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Hibiki
+//
+//  Created by emp-private-mac-suraj on 04/07/20.
+//  Copyright © 2020 Goodpatch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

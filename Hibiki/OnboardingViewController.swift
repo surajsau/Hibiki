ee62ae808ad88559f753d0840b0d1689e1289e82
suraj <i@surajsau.in>
2020-07-04 09:37:25 +0530

    :electric_plug: Added Onboarding storyboard

---
+++ Hibiki/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding.swift
+//  Hibiki
+//
+//  Created by emp-private-mac-suraj on 04/07/20.
+//  Copyright © 2020 Goodpatch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
